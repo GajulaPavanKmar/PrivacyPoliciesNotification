9963e2ae3c0cb0c06c34219f6f9811be364430d4
GajulaPavanKmar <pavangajula03@gmail.com>
2024-03-22 12:37:20 -0400

    Initial commit

---
+++ src/main/java/com/privacypolicies/PrivacyPoliciesNotification/PrivacyPoliciesNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.privacypolicies.PrivacyPoliciesNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyPoliciesNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyPoliciesNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/privacypolicies/PrivacyPoliciesNotification/PrivacyPoliciesNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privacypolicies.PrivacyPoliciesNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacyPoliciesNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
